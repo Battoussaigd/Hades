4784255d7835945f7502a1162b7b00443339a50e
Battoussaigd <claudioegdiaz@gmail.com>
2026-05-03 03:59:35 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    HADES 2.1 — Service Worker (Offline First)
    ═══════════════════════════════════════════ */
 
-const CACHE_NAME = 'hades-v2.1.14';
+const CACHE_NAME = 'hades-v2.1.15';
 const ASSETS = [
   '/',
   '/index.html',
